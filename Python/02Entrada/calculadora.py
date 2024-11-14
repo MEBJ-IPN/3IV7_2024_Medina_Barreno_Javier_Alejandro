da913f97d3dc212ed38843124cb880b6142a920b
MEBJ-IPN <jmedinab2300@alumno.ipn.mx>
2024-11-13 22:14:56 -0600

    Create calculadora.py

---
+++ Python/02Entrada/calculadora.py
@@ -0,0 +1,11 @@
+#crear una calculadora sencilla
+#para la entrada de datos es necesario utilizar el metodo input
+
+numero1 = float(input("Introduce el primer número: "))
+numero2 = float(input("Introduce el segundo número: "))
+
+#sumar
+
+suma = numero1+numero2
+
+print("La suma es de: ", suma)
