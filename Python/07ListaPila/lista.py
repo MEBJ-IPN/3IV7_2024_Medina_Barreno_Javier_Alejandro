3163d9e032b0788e2c5c69c2a728698bd9f3a6f6
MEBJ-IPN <jmedinab2300@alumno.ipn.mx>
2024-11-20 17:30:45 -0600

    Create lista.py

---
+++ Python/07ListaPila/lista.py
@@ -0,0 +1,7 @@
+estudiantes = ["Ana", "Hugo", "Paco", "Luis"]
+
+estudiantes.append("Diana")
+print("Lista de estudiantes: ", estudiantes)
+
+estudiantes.remove("Hugo")
+print("Lista actualizada: ", estudiantes)
